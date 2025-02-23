41ef90c4d6a269c6bb232b0942081137aa1ce49f
vibin chemmannoor <vibinchemmannoor@gmail.com>
2025-02-23 12:09:13 +0530

    first commit

---
+++ server/config/mongodb.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+const connectDB = async ()=>{
+    mongoose.connection.on('connected',()=>console.log('data base connected'));
+await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+}
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb.js';
+
+
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 4000 ; 
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials:true}));
+
+app.get('/',(req,res)=> res.send("API working"))
+
+app.listen(port, ()=>console.log(`Server started on port : ${port}`));
